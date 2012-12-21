ee535361bdc79a31cee1f550e446196e0287965a
jhfrintrop <jhfrintrop@t-online.de>
2012-12-21 18:23:50 +0100

    Kommentiert

--- bit_access.c
+++ bit_access.c
@@ -1,20 +1,20 @@
 #include "bit_access.h"
 
-void set_bit(BYTE *ptr, int index, BOOL value)
-{
-    BYTE mask = (BYTE) 1 << (index%8);
-    ptr[index/8] =  (ptr[index/8] & ~mask) | (value << (index%8));
-}
-
 BOOL get_bit(BYTE *ptr, int index)
 {
-    return (ptr[index/8] >> index%8) & 1;
+    return (ptr[index / 8] >> index % 8) & 1;
+}
+
+void set_bit(BYTE *ptr, int index, BOOL value)
+{
+    BYTE mask = (BYTE) 1 << (index % 8);
+    ptr[index / 8] =  (ptr[index / 8] & ~mask) | (value << (index % 8));
 }
 
 void print_bits(BYTE *ptr, int offset, int length)
 {
     int i;
-    for(i = offset; i < offset+length; i++)
+    for(i = offset; i < (offset + length); i++)
     {
         printf("%d", get_bit(ptr, i));
     }
