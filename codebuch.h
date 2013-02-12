129dea5cae3b7aa3dc1e243698dac5b08d37ada1
jhfrintrop <jhfrintrop@t-online.de>
2013-02-12 14:53:06 +0100

    Kommentare im Codebuch-Header nochmal überarbeitet

--- codebuch.h
+++ codebuch.h
@@ -17,56 +17,56 @@ extern CODEBUCH* codebuch_new_from_frequency(unsigned int f[256]);
 
 /**
  * Erstellt eine neues Codebuch aus einem Bitarray.
- * @param ba BITARRAY* Das Bitarray.
+ * @param p_ba BITARRAY* Das Bitarray.
  * @return CODEBUCH* Das neue Codebuch oder NULL im Fehlerfall.
  */
-extern CODEBUCH* codebuch_new_from_structur(BITARRAY* ba);
+extern CODEBUCH* codebuch_new_from_structur(BITARRAY* p_ba);
 
 /**
  * Gibt das Codebuch frei.
- * @param cb CODEBUCH** Das freizugebende Codebuch.
+ * @param pp_cb CODEBUCH** Ein Zeiger auf das freizugebende Codebuch.
  */
-extern void codebuch_free(CODEBUCH** cb);
+extern void codebuch_free(CODEBUCH** pp_cb);
 
 /**
  * Gibt den Code zu einem Zeichen zurück.
- * @param cb CODEBUCH* Das zu durchsuchende Codebuch.
+ * @param p_cb CODEBUCH* Das zu durchsuchende Codebuch.
  * @param c unsigned char Das zu suchende Zeichen.
  * @return BITARRAY* Der Code zu dem Zeichen als Bitarray oder NULL im Fehlerfall.
  */
-extern BITARRAY* codebuch_code_for_char(CODEBUCH* cb, unsigned char c);
+extern BITARRAY* codebuch_code_for_char(CODEBUCH* p_cb, unsigned char c);
 
 /**
  * Gibt das Zeichen zu einem Code zurück.
- * @param cb CODEBUCH* Das Codebuch.
- * @param c BITARRAY* Der zu suchende Code als Bitarray.
- * @param used_bits unsigned int* Wenn != NULL, wird hier die Anzahl der benutzten Bits gespeichert.
+ * @param p_cb CODEBUCH* Das Codebuch.
+ * @param p_code BITARRAY* Der zu suchende Code als Bitarray.
+ * @param p_used_bits unsigned int* Wenn != NULL, wird hier die Anzahl der benutzten Bits gespeichert.
  * @return unsigned char Das Zeichen zu dem Code oder blablabla im Fehlerfall.
  */
-extern unsigned char codebuch_char_for_code(CODEBUCH* cb, BITARRAY* c, unsigned int* used_bits);
+extern unsigned char codebuch_char_for_code(CODEBUCH* p_cb, BITARRAY* p_code, unsigned int* p_used_bits);
 
 /**
  * Speichert ein Codebuch als Bitarray.
- * @param cb CODEBUCH* Das abzuspeichernde Codebuch.
+ * @param p_cb CODEBUCH* Das abzuspeichernde Codebuch.
  * @return BITARRAY* Das Codebuch als Bitarray.
  */
-extern BITARRAY* codebuch_structure(CODEBUCH* cb);
+extern BITARRAY* codebuch_structure(CODEBUCH* p_cb);
 
 #ifdef TEST
 /**
  * Prüft, ob 2 Codebücher gleich sind.
- * @param cb1 CODEBUCH* Das eine Codebuch.
- * @param cb2 CODEBUCH* Das andere Codebuch.
+ * @param p_cb1 CODEBUCH* Das eine Codebuch.
+ * @param p_cb2 CODEBUCH* Das andere Codebuch.
  * @return BOOL TRUE, falls cb1 und cb2 gleich sind, FALSE sonst.
  */
-extern BOOL codebuch_equals(CODEBUCH* cb1, CODEBUCH* cb2);
+extern BOOL codebuch_equals(CODEBUCH* p_cb1, CODEBUCH* p_cb2);
 
 /**
  * Gibt die Größe des Codebuchs zurück.
- * @param cb CODEBUCH* Das Codebuch.
+ * @param p_cb CODEBUCH* Das Codebuch.
  * @return unsigned int Die Größe des Codebuchs.
  */
-extern unsigned int codebuch_size(CODEBUCH* cb);
+extern unsigned int codebuch_size(CODEBUCH* p_cb);
 #endif
 
 #endif
