4fddfe2874f64203b9b8a37a137e94812d4db57d
jhfrintrop <jhfrintrop@t-online.de>
2013-02-18 22:25:33 +0100

    farben hinzugefügt, existenz der testsuit überprüft

--- tests/test.sh
+++ tests/test.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
+NORMAL="\033[0m"
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+
 suits="bitarray bitfile heap code frequency codebuch diffs.sh"
 
 errors=0
 
-if [ -e tests/bitarray ]; then
-    cd tests
-fi
+cd tests
 
 for suit in $suits; do
-    ./${suit}
-    result=$?
-    if [ $result -gt 0 ]; then
-        errors=$(($errors + $result))
-        echo "Error(s) in  \"${suit}\""
+	if [ ! -e ./${suit} ]; then
+		echo -e ${YELLOW}"Warning: Testsuit ${suit} does not exist! Skipping"${NORMAL}
+	else
+    	./${suit}
+    	result=$?
+    	if [ $result != 0 ]; then
+        	errors=$(($errors + $result))
+        	echo -e ${RED}"Error(s) in  \"${suit}\""${NORMAL}
+    	fi;
     fi;
 done;
 
 echo; echo;
 
 if [ $errors = 0 ]; then
-    echo "All tests passed";
+    echo -e ${GREEN}"All tests passed"${NORMAL}
 else
-    echo "${errors} tests failed"
+    echo -e ${RED}"${errors} tests failed"${NORMAL}
 fi;
